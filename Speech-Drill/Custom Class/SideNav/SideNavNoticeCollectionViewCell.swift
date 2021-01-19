561c7c9e006068638733828fc35c6e4da3c63c82
Parth Tamane <ptamane3@gatech.edu>
2021-01-19 22:49:00 +0530

    added collection view for notices and headers in side nav

---
+++ Speech-Drill/Custom Class/SideNav/SideNavNoticeCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SideNavNoticeCollectionViewCell.swift
+//  Speech-Drill
+//
+//  Created by Parth Tamane on 19/01/21.
+//  Copyright © 2021 Parth Tamane. All rights reserved.
+//
+
+import Foundation
---
+++ Speech-Drill/Model/NoticeStructure.swift
@@ -0,0 +1,9 @@
+//
+//  NoticesStructure.swift
+//  Speech-Drill
+//
+//  Created by Parth Tamane on 19/01/21.
+//  Copyright © 2021 Parth Tamane. All rights reserved.
+//
+
+import Foundation
